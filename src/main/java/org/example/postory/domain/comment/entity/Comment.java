f1daa11125e836b048d558001a6194cdeeef5820
정소민 <yjk70702002@gmail.com>
2025-04-07 22:00:52 +0900

    feat: 엔티티 생성

--- src/main/java/org/example/postory/domain/comment/entity/Comment.java
+++ src/main/java/org/example/postory/domain/comment/entity/Comment.java
@@ -1,5 +1,34 @@
 package org.example.postory.domain.comment.entity;
 
-public class Comment {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.example.postory.domain.post.entity.Post;
+import org.example.postory.domain.user.entity.User;
+import org.example.postory.global.common.BaseEntity;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private int commentLikeCount = 0;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
