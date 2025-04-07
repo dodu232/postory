c99e97d2bd1965cddc15e585d606f4ff9c139ceb
dodu232 <dodu7510@gmail.com>
2025-04-07 15:57:20 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/org/example/postory/PostoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.postory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/postory/PostoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.postory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
