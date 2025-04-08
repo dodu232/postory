06da1dfbeec2afc9354bd3a4255d521661dce6d0
dodu232 <dodu7510@gmail.com>
2025-04-08 21:39:11 +0900

    refactor: UserService interface 구현하도록 수정

--- src/main/java/org/example/postory/domain/user/service/UserService.java
+++ src/main/java/org/example/postory/domain/user/service/UserService.java
@@ -1,41 +1,12 @@
 package org.example.postory.domain.user.service;
 
-import lombok.RequiredArgsConstructor;
 import org.example.postory.domain.user.entity.User;
-import org.example.postory.domain.user.repository.UserRepository;
-import org.example.postory.global.error.ApiException;
-import org.example.postory.global.error.response.ErrorType;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository repository;
+    String getRefreshToken(long id);
 
-    /**
-     * refreshToken 가져오기
-     */
-    public String getRefreshToken(long id){
-        User findUser = repository.findById(id)
-            .orElseThrow(() -> new ApiException(ErrorType.USER_NOT_FOUND));
-        return findUser.getRefreshToken();
-    }
+    void saveToken(long id, String refreshToken);
 
-    /**
-     * refreshToken 저장
-     */
-    @Transactional
-    public void saveToken(long id, String refreshToken){
-        User findUser = repository.findById(id)
-            .orElseThrow(() -> new ApiException(ErrorType.USER_NOT_FOUND));
-        findUser.updateToken(refreshToken);
-    }
-
-    public User getByEmail(String email){
-        return repository.findByEmail(email)
-            .orElseThrow(() -> new ApiException(ErrorType.EMAIL_NOT_FOUND));
-    }
+    User getByEmail(String email);
 }
