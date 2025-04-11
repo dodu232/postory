5f9f7aab61ced177fb21f7180b4f5cde857ce013
정소민 <yjk70702002@gmail.com>
2025-04-11 19:08:59 +0900

    fix: SignupRequestDto에 message 추가

--- src/main/java/org/example/postory/domain/user/dto/SignupRequestDto.java
+++ src/main/java/org/example/postory/domain/user/dto/SignupRequestDto.java
@@ -18,11 +18,11 @@ public class SignupRequestDto {
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
         message = "비밀번호는 대소문자, 숫자, 특수문자를 포함한 8자 이상이어야 합니다."
     )
-    @Size(max = 20)
+    @Size(max = 20, message = "비밀번호는 20자 이하여야 합니다.")
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 
-    @Size(max = 20)
+    @Size(max = 20, message = "이름은 20자 이하여야 합니다.")
     @NotBlank(message = "이름은 필수입니다.")
     private String name;
 
