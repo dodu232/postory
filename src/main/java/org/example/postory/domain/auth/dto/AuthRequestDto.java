08947bbd31196da88f053c6a31da76d5a393e67d
dodu232 <114296489+dodu232@users.noreply.github.com>
2025-04-12 15:29:55 +0900

    refactor: login valid 추가 (#80)

--- src/main/java/org/example/postory/domain/auth/dto/AuthRequestDto.java
+++ src/main/java/org/example/postory/domain/auth/dto/AuthRequestDto.java
@@ -1,6 +1,9 @@
 package org.example.postory.domain.auth.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,9 +13,15 @@ public class AuthRequestDto {
     @AllArgsConstructor
     public static class Login {
 
-        @Email
+        @Email(message = "유효한 이메일 형식이어야 합니다.")
         private String email;
 
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "비밀번호는 대소문자, 숫자, 특수문자를 포함한 8자 이상이어야 합니다."
+        )
+        @Size(max = 20, message = "비밀번호는 20자 이하여야 합니다.")
+        @NotBlank(message = "비밀번호는 필수입니다.")
         private String password;
     }
 }
